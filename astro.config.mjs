fc0b2e591290c410b118c64e165006a3355ae83e
Óscar González Fresno <oscargonzalezfresno@gmail.com>
2026-03-30 19:54:03 +0200

    Build on Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
-  adapter: node({
-    mode: 'standalone'
-  })
+  output: 'static',
+  adapter: vercel()
 });
